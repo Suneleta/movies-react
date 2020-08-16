f01c022e37a3048e0cd6ded4e86c80e269cc31ad
Eleni Taki <elenitaki@col.vueling.com>
2020-08-16 16:45:17 +0200

    Updates

---
+++ precache-manifest.af50797c90f80d20403a30b24a8c9970.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace8e3cac9bef7cc7b51d0726c7a0a2b",
+    "url": "/movies-react/index.html"
+  },
+  {
+    "revision": "74e847904308abfad14c",
+    "url": "/movies-react/static/css/main.24cd1f37.chunk.css"
+  },
+  {
+    "revision": "54c7de154e0766add9a0",
+    "url": "/movies-react/static/js/2.0242cdbc.chunk.js"
+  },
+  {
+    "revision": "74e847904308abfad14c",
+    "url": "/movies-react/static/js/main.1f22cec5.chunk.js"
+  },
+  {
+    "revision": "130e1eec716e483c7fb6",
+    "url": "/movies-react/static/js/runtime-main.1800d15e.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/movies-react/static/media/loader.26ef8bb4.gif"
+  }
+]);
